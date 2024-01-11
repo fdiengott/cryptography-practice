a33a8b8a6e08bc9833d940f51e6244247c04023c
fdiengott <freddy.diengott@proton.me>
2024-01-11 11:23:32 -0500

    add some explanatory comments

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const {
     timingSafeEqual,
 } = require("crypto");
 
+// : basic hashing function. using the blake2s function optimized for 32-bit microprocessors.
+// : wanted to try a different function than sha256, which I believe to be the standard
 const hash = input => {
     return createHash("blake2s256").update(input).digest("base64");
 };
 
+// : adding a salt to a hash to give an added layer of security
+// : normally the salt and hash would be stored in a database
 const saltedHash = input => {
     const salt = randomBytes(16).toString("base64");
 
@@ -18,6 +22,7 @@ const saltedHash = input => {
     return `${salt}:${hashedPassword}`;
 };
 
+// : a terminal interface to input passwords and have hashes outputted
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
